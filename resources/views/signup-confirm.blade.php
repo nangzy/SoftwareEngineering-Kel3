df17317929154c5f1eabbea77c216bcf90e5f5a0
nangzy <nicholaus@binus.ac.id>
2023-05-10 22:48:56 +0700

    #1-login-add page: forgotpw-confirm and signup-confirm

---
+++ resources/views/signup-confirm.blade.php
@@ -0,0 +1,35 @@
+@extends('template1')
+
+@section('content')
+    <div class="container-fluid d-flex flex-column justify-content-center col-10 mt-5">
+        <div class="fs-1 d-flex justify-content-center mb-2">
+            <div style="color: #ffffff">
+                Get Your&nbsp;
+            </div>
+            
+            <div class="tulisan-merah-terang">
+                E-SCORE TV&nbsp;
+            </div>
+            <div style="color: #ffffff">
+                Account
+            </div>
+        </div>
+
+        <div class="fs-6 d-flex justify-content-center mb-4" style="color: #A3B8BF">
+            Sign into The E-score TV with your EST account
+        </div>
+        
+        <div class="container-fluid rounded-4 d-flex flex-column col-4 warna-abu" style="background-color: #5a2225">
+            <div class="justify-content-center d-flex mt-5">
+                <img src="/images/icons/clipboardtick.png" alt="clipboard" class="col-4">
+            </div>
+
+            <div class="justify-content-center d-flex mb-4 fs-2 text-center mt-3 fw-semibold">
+                You have successfully sign up.
+            </div>
+            <div class="d-flex justify-content-center mb-2  ">
+                <button type="submit" class="btn btn-lg btn-danger shadow m-3 fw-semibold" style="color:black">Log In</button>
+            </div>
+        </div>
+    </div>
+@endsection
