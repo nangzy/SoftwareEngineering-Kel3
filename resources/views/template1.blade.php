20e0b3dafde6576ae910487b657651f66467a4b2
Ariel Sefrian <ariel.sefrian@binus.edu>
2023-05-20 15:43:15 +0700

    #2-homepage-finished footer

--- resources/views/template1.blade.php
+++ resources/views/template1.blade.php
@@ -15,10 +15,10 @@
     {{-- header --}}
     <div class="container d-flex justify-content-between pt-3 align-items-center">
         <div class="warna-krem fs-4 d-flex flex-row align-items-end">
-            <img src="/images/logo1.png" alt="logo" class="img-fluid">
+            <img src="{{ asset('/images/logo1.png') }}" alt="logo" class="img-fluid">
             &nbsp;E-SCORE TV
         </div>
-    
+
         <div>
             @yield('header')
         </div>
@@ -26,13 +26,43 @@
 
     @yield('content')
 
+    <br><br>
+
     {{-- footer --}}
+    <img src="{{ asset('/images/others/footer-transparent-2.png') }}" alt="" style="position: absolute; background-repeat: no-repeat; z-index: -10; object-fit: fill; height: 60vh; width: 100%;">
     <div class="container-fluid warna-abu col-11 mt-5 mb-3">
-        <div class="fs-6">
-            2023 © E-SCORE TV
-        </div>
-        <div class="fs-6">
-            E-Score TV isn't endorsed by all the games we present and doesn't reflect the views or opinions of anyone officially involved in producing or managing all the games. All associated properties are trademarks or registered trademarks of the company that develops the games.
+        <br><br><br><br><br><br><br><br><br>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="fs-6">
+                    2023 © <u>E-SCORE TV</u>
+                </div>
+                <br>
+                <div class="fs-6">
+                    E-Score TV isn't endorsed by all the games we present and doesn't reflect the views or opinions of anyone officially involved in producing or managing all the games. All associated properties are trademarks or registered trademarks of the company that develops the games.
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div style="color: white; font-size: 20pt; font-weight: bolder; text-align: right;">
+                    FOLLOW US
+                </div>
+
+                <br>
+
+                <div style="text-align: right;">
+                    <a href="#" class="pressed-icons" style="display: inline-block; margin-right: 1rem;">
+                        <img src="{{ asset('images/Icons/twitter.png') }}" alt="twitter" style="height: 3rem;">
+                    </a>
+
+                    <a href="#" class="pressed-icons" style="display: inline-block; margin-right: 1rem;">
+                        <img src="{{ asset('images/Icons/instagram.png') }}" alt="twitter" style="height: 3rem;">
+                    </a>
+
+                    <a href="#" class="pressed-icons" style="display: inline-block;">
+                        <img src="{{ asset('images/Icons/discord.png') }}" alt="twitter" style="height: 3rem;">
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </body>
